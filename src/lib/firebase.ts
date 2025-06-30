d3891db47762fd6d4a02c3a1d18543561325d56c
Ali Nuryadin <nuryadinali1@gmail.com>
2025-06-30 11:59:29 +0000

    buatkan envnya untuk di vercel

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD1sTMLEJUoRziGCekKsYYksVQfIgSFRv8",
-  authDomain: "hutang-42718.firebaseapp.com",
-  projectId: "hutang-42718",
-  storageBucket: "hutang-42718.appspot.com",
-  messagingSenderId: "1079155791553",
-  appId: "1:1079155791553:web:492ca19aace275f0ad2640"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
